14782010816d7cad12fdb1401dac05fc07d518da
rat1001rat-dev <rat1001rat@gmail.com>
2025-09-13 22:17:57 +0300

    Update __main__.py

--- yamenthon/__main__.py
+++ yamenthon/__main__.py
@@ -72,7 +72,7 @@ async def startup_process():
 
     async def start_bot():
       try:
-          List = ["YamenThon","YamenThon_Gorop","YamenThon_vars","Q_A_VI","Binance_gift1","YamenThon_cklaish","YamenThon1", "P_8_8I"]
+          List = ["YamenThon","YamenThon_Gorop","YamenThon_vars","Q_A_VI","YamenThon_support","YamenThon_cklaish","YamenThon1", "P_8_8I"]
           from telethon.tl.functions.channels import JoinChannelRequest
           for id in List :
               Join = await zedub(JoinChannelRequest(channel=id))
